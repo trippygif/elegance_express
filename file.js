aa4444da29f23ecbb7a892b3af42030945cd98d4
Bryan Yoelin <bryoelin@gmail.com>
2016-08-26 12:06:01 -0600

    accounted for new test cases

--- file.js
+++ file.js
@@ -7,7 +7,14 @@ var express = require('express');
 
 //call the elegence module. this should make a 'require' var
 elegance.initialize(express, ['modules', 'objects'], ['routes']);
-helloWorld.speak(4);
 
+
+
+// TEST CASES
+helloworld.speak(4);
+goodbyeworld.speak("Bryan");
+hello_object.speak(5);
+randomobject.add(3, 7);
+//EVAL
 eval("var newTest = 'Hi';");
 console.log(newTest);
